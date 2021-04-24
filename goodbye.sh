3480e33bc627d07738c1a8a9234746683bbac77f
nikizhelqzkov <nikizhelqzkov@Asus.localdomain>
2021-04-24 21:45:33 +0300

    Funny shell file

---
+++ goodbye.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Hello
+sleep 2
+echo How are you
+sleep 1
+echo Do you love funny jokes
+sleep 2
+echo It\'s time to see one of them
+sleep 1
+echo ...1
+sleep 1
+echo ...2
+sleep 2
+echo THREEE
+sleep 2
+echo BUUUM BYEEEE
+sleep 1
+shutdown -p
